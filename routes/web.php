0986c4ff113f33d196ec04d23d0db3e559aa7a63
tranhanh18042 <hanhtran18042@gmial.com>
2023-05-11 15:54:47 +0700

    Add middleware

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('login');
-});
 Route::get('/login', [LoginController::class, 'index']);
 Route::post('/login', [LoginController::class, 'login']);
+
+Route::middleware([Login::class])->group(function () {
+});
